dc94f225f6f4b1a3aa8b8b3bb608fb9835c5305f
WIKKO <dalineko@googlemail.com>
2023-08-02 17:07:09 +0200

    Delete discogs-collection-folders/discogs-collection-folders directory

--- discogs-collection-folders/discogs-collection-folders/includes/enqueue-scripts.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Enqueue jQuery from the WordPress core
-function discogs_collection_folders_enqueue_scripts() {
-    wp_enqueue_script( 'jquery' );
-}
-add_action( 'wp_enqueue_scripts', 'discogs_collection_folders_enqueue_scripts' );
-?>
--- discogs-collection-folders/discogs-collection-folders/includes/enqueue-styles.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Enqueue CSS file(s)
-function discogs_collection_folders_enqueue_styles() {
-    wp_enqueue_style( 'discogs-collection-folders-style', plugins_url( 'includes/CSS/discogs-collection-folders.css', __FILE__ ) );
-}
-add_action( 'wp_enqueue_scripts', 'discogs_collection_folders_enqueue_styles' );
-?>
